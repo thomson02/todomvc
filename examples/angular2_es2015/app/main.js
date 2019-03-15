c00020fbeda775949c2e03af7ee27f935685a574
EMEA\i670426 <andrew.r.thomson@jpmorgan.com>
2019-03-15 14:36:42 +0000

    random changes

--- examples/angular2_es2015/app/main.js
+++ examples/angular2_es2015/app/main.js
@@ -8,5 +8,3 @@ import 'rxjs';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { MainModule } from './main.module';
-
-platformBrowserDynamic().bootstrapModule(MainModule);
